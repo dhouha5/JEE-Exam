216c00f02e69fca09fe467d56c5e87396b1c374a
dhouha5 <drididhouha462@gmail.com>
2024-05-10 00:02:45 +0100

    first commit

---
+++ src/main/java/tn/iit/print/RegistrationServlet.java
@@ -0,0 +1,66 @@
+package tn.iit.print;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String uname = request.getParameter("name");
+        String uemail = request.getParameter("email");
+        String upwd = request.getParameter("pass");
+        String umobile = request.getParameter("contact");
+        RequestDispatcher dispatcher = null;
+        Connection con = null;
+        try {
+            // **Assuming you're using MySQL Connector/J (replace if necessary)**
+        	Class.forName("com.mysql.jdbc.Driver"); // Update class name based on your driver
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/imprimante", "root", "");
+            PreparedStatement pst = con.prepareStatement("insert into users(uname,upwd,uemail,umobile) values(?,?,?,?)");
+            pst.setString(1, uname);
+            pst.setString(2, upwd);
+            pst.setString(3, uemail);
+            pst.setString(4, umobile);
+
+            int rowCount = pst.executeUpdate();
+            dispatcher = request.getRequestDispatcher("registration.jsp");
+            if (rowCount > 0) {
+                request.setAttribute("status", "success");
+            } else {
+                request.setAttribute("status", "failed");
+            }
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
